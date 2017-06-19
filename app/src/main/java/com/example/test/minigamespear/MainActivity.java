51407062cf43095ea03917e6a29e7f14d2eff3f8
nastyabadikova <nastyabadikova@yandex.ru>
2017-06-19 14:06:16 +0300

    Initial commit Main menu with super ultra cool design

--- app/src/main/java/com/example/test/minigamespear/MainActivity.java
+++ app/src/main/java/com/example/test/minigamespear/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.test.minigamespear;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
