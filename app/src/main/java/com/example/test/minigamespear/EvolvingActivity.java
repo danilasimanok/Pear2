27ea05f859f459066da6bc00560cb0dbc9ec84ef
danilasimanok <dsimanok@bk.ru>
2017-06-19 12:06:25 +0400

    Пробуем хорошие сохранения.

---
+++ app/src/main/java/com/example/test/minigamespear/EvolvingActivity.java
@@ -0,0 +1,13 @@
+package com.example.test.minigamespear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EvolvingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evolving);
+    }
+}
---
+++ app/src/main/java/com/example/test/minigamespear/EvolvingView.java
@@ -0,0 +1,24 @@
+package com.example.test.minigamespear;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Андрей on 19.06.2017.
+ */
+
+public class EvolvingView extends View {
+    public EvolvingView(Context context) {
+        super(context);
+    }
+    @Override
+    protected void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
+    }
+}
