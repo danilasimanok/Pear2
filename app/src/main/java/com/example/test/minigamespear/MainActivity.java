1f48c0eb06582d874bba97d397186cb2a209e9b7
AkimPopelishko <akimzaya@mail.ru>
2017-06-20 14:05:39 +0300

    новая ветка

--- app/src/main/java/com/example/test/minigamespear/MainActivity.java
+++ app/src/main/java/com/example/test/minigamespear/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, GallowsActivity.class);
-                //intent.putExtra(data_id,0);dfshdfgh
+                //intent.putExtra(data_id,0);
                 startActivity(intent);
             }
         };
