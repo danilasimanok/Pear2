b62a390172df4a7e569921516c1247b8ae6e4be4
nastyabadikova <nastyabadikova@yandex.ru>
2017-06-21 13:37:52 +0300

    Merge remote-tracking branch 'origin/Danechka' into master1
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/test/minigamespear/MainActivity.java
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/test/minigamespear/EvolvingActivity.java
+++ app/src/main/java/com/example/test/minigamespear/EvolvingActivity.java
@@ -1,7 +1,12 @@
 package com.example.test.minigamespear;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Button;
+import android.view.View;
+
 
 public class EvolvingActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class EvolvingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evolving);
+
+        Button eYC =(Button)findViewById(R.id.button);
+        eYC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EvolvingActivity.this, StatisticMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
