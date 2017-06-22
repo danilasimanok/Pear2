3dd3052e6610c22ce6f3edc02ba101d3fc0b1315
Igor Sher <work.iggs@gmail.com>
2017-06-22 13:20:07 +0300

    menu button added; fixed chronometer issues

--- app/src/main/java/com/example/test/minigamespear/DSLWactivity.java
+++ app/src/main/java/com/example/test/minigamespear/DSLWactivity.java
@@ -1,5 +1,6 @@
 package com.example.test.minigamespear;
 // test 2
+import android.content.Intent;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ import java.util.Random;
 public class DSLWactivity extends AppCompatActivity {
 
     private int c = 0;
+    private int cold = 0;
     private final String tag = "dslw";
     private String word;
     int[] array = new int[100];
@@ -42,30 +44,43 @@ public class DSLWactivity extends AppCompatActivity {
             array[i] = a;
         }
 
-        for(int i=0;i<100;i++){
-            Log.d(tag,array[i]+"");
-        }
-
 
         Resources res = getResources();
         final TypedArray words = res.obtainTypedArray(R.array.words);
         final Chronometer chrono = (Chronometer)findViewById(R.id.chrono);
         Button dSLW =(Button)findViewById(R.id.DSLW);
+        Button menu = (Button)findViewById(R.id.menu);
         final TextView textView = (TextView) findViewById(R.id.textView);
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DSLWactivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         dSLW.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 long elapsedMillis = SystemClock.elapsedRealtime()
                         - chrono.getBase();
-                Log.d(tag,"clicked");
                 if (c == 0){
+                    chrono.setBase(SystemClock.elapsedRealtime());
                     chrono.start();
                 }
                 if (c == 100 | elapsedMillis > 60000 ) {
                     chrono.stop();
-                    textView.setText("Ход следующего игрока, ваш счёт: " +c+", ваше время: "+(int)(elapsedMillis/1000) + " секунд(ы)");
+                    if (cold != 0) {
+                        textView.setText("Ход следующей команды, ваш счёт: " + c + ", ваше время: " + (int) (elapsedMillis / 1000) + " секунд(ы), счёт предыдущей команды: "+cold);
+                    }
+                    else {
+                        textView.setText("Ход следующей команды, ваш счёт: " + c + ", ваше время: " + (int) (elapsedMillis / 1000) + " секунд(ы)");
+                    }
+
+                    cold = c;
                     c = 0;
-                    chrono.setBase(SystemClock.elapsedRealtime());
+
                 }
 
                 else {
